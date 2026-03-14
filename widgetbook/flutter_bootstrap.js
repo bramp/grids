0f230fa1b2239bd91b4850805efb9e9671a686ac
bramp <bramp@users.noreply.github.com>
2026-03-14 23:47:56 +0000

    deploy: 1b229efec11d434a9d8e02bd63eedc81359a8bed

--- widgetbook/flutter_bootstrap.js
+++ widgetbook/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4067155242" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1438389037" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
